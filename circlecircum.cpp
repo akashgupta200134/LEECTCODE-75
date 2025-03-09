fb55b0dae8e9ba66d8f5deca04be839b96d89ce1
Akash Gupta <akashgupta_ce_20212@ltce.in>
2025-03-10 02:51:30 +0530

    circumferenceof thecircle

---
+++ circlecircum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std ; 
+
+int main() {
+ float pi = 3.14;
+ float R ; 
+ cout << "Enter the Radius of the circle" << endl ;
+ cin >> R ;
+
+  float circumference  =  2*pi*R;
+
+cout << "circumference of the circle is : " << circumference << endl ;
+
+return 0 ; 
+
+
+}
